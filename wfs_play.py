fefc67c0e47447044e3ca584347ff9f5cbfa4b07
Kim Nes Leirvik <kim.leirvik@niva.no>
2021-09-29 20:28:46 +0200

    test intersects

--- wfs_play.py
+++ wfs_play.py
@@ -62,14 +62,14 @@ polygon_node = feature_collection.xpath('//*[local-name()="Polygon"]')[0]
 csw_polygon = parser.from_bytes(etree.tostring(polygon_node), csw.Polygon)
 # %%
 q_records = csw.GetRecords(
-    result_type="hits",
+    result_type="results",
     query=csw.Query(
         type_names=["csw:Record"],
         element_set_name="full",
         constraint=csw.Constraint(
             version="1.1.0",
             filter=csw.Filter(
-                within=csw.Within(
+                intersects=csw.Intersects(
                     property_name=["ows:BoundingBox"], polygon=csw_polygon
                 )
             ),
@@ -78,14 +78,14 @@ q_records = csw.GetRecords(
 )
 #%%
 resp = requests.post(
-    GEONORGE_HOST,
+    MET_HOST,
     headers={"Content-Type": "application/xml"},
     data=serializer.render(q_records, ns_map={"csw": "http://www.opengis.net/cat/csw/2.0.2"}),
 )
 #%%
 records = parser.from_string(resp.text, csw.GetRecordsResponse)
 #%%
-records.search_results.number_of_records_matched
+records.search_results
 #%%
 wfs_client = WebFeatureService(MILJO_HOST)
 #%%
