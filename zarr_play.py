647a857de1d7e8490f4afbea770b13fae327e4ff
Kim Nes Leirvik <kim.leirvik@niva.no>
2021-12-15 15:22:03 +0100

    add zarr example

--- zarr_play.py
+++ zarr_play.py
@@ -1,9 +1,17 @@
 #%%
+import gcsfs
 import xarray as xr
+import zarr
 # %%
 ds = xr.open_mfdataset('./nc/*.nc')
 # %%
 ds
 # %%
-ds.to_zarr('zarr/datasets', consolidated=True)
+gcs_path = 'gs://niva-test-datasets/iddefjorden'
+gcs = gcsfs.GCSFileSystem()
+store = gcsfs.GCSMap(root=gcs_path, gcs=gcs)
+#%%
+compressor = zarr.Blosc(cname='zstd')
+encoding = {vname: {'compressor': compressor} for vname in ds.data_vars}
+ds.to_zarr(store=store, consolidated=True)
 # %%
