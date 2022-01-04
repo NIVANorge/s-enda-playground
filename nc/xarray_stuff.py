1fb9096a8b0d6db73465a8d92b6c1fb88a596419
Kim Nes Leirvik <kim.leirvik@niva.no>
2022-01-04 15:27:40 +0100

    clean example

--- nc/xarray_stuff.py
+++ nc/xarray_stuff.py
@@ -13,30 +13,31 @@ from datetime import datetime
 import json
 #%%
 w_temp = WaterTemp.new(
-    [[10, 15], [20, 25]],
+    #(4m, 8m)
+    [(10, 15), (20, 25)],
     time=["1970-01-01T00:00:00.000000000", "1970-01-01T01:00:00.000000000"],
     depth=[4, 8],
 )
 #%%
 sal = Salinity.new(
-    [[10, 150], [15, 20]],
+    [(10, 1500), (15, 2000)],
     time=["1970-01-01T00:00:00.000000000", "1970-01-01T10:00:00.000000000"],
     depth=[4, 8],
 )
 #%%
 con = Conductivity.new(
-    [[10, 15], [15, 20]],
+    [(10, 15), (15, 20)],
     time=["1970-01-01T00:00:00.000000000", "1970-01-01T10:00:00.000000000"],
     depth=[4, 8],
 )
 #%%
 ds = xr.merge([w_temp, sal, con])
 #%%
+ds.attrs = CTDDatasetAttributes("hei", str(datetime.now()), keywords=["hei"]).__dict__
+#%%
 ds.to_netcdf('test.nc')
 #%%
-ds.attrs = CTDDatasetAttributes("hei", datetime.now(), keywords=["hei"]).__dict__
-#%%
-ds.sel(depth=8)["sea_water_temperature"].plot.line('o')
+ds.sel(depth=4).salinity.plot.line('o')
 
 # %%
 ds_e39 = xr.open_dataset('https://thredds.met.no/thredds/dodsC/obs/buoy-svv-e39/2022/01/202201_E39_G_Halsafjorden_ctd.nc')
