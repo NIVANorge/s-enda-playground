ff339d623ef05dfc6bfa793c7626e8b20c6495d6
Kim Nes Leirvik <kim.leirvik@niva.no>
2022-01-12 20:24:14 +0100

    traj attributes

--- hello-xarray/cf_classes/trajectory.py
+++ hello-xarray/cf_classes/trajectory.py
@@ -7,13 +7,11 @@ from xarray_dataclasses import Attr, Coordof, Data, Name
 from cf_classes.literals import TIME
 
 from cf_classes.attributes import (
-    AltitudeAttrs,
     DatasetAttrs,
-    LatitudeAttrs,
-    LongitudeAttrs,
 )
 
 from cf_classes.common import TimeAxis
+
 
 @dataclass
 class TrajectoryId:
@@ -21,11 +19,12 @@ class TrajectoryId:
     cf_role: Attr[str] = "trajectory_id"
     long_name: Attr[str] = "trajectory"
 
+
 @dataclass
-class _TimeSeriesAttrs:
+class _TrajectoryAttrs:
     station_name: Attr[str]
 
 
 @dataclass
-class TimeSeriesAttrs(DatasetAttrs, _TimeSeriesAttrs):
+class TrajectoryAttrs(DatasetAttrs, _TrajectoryAttrs):
     pass
