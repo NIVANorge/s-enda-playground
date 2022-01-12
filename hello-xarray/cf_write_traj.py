abcb1b7b2a265a0787215cc61852ab96c83f4c3b
Kim Nes Leirvik <kim.leirvik@niva.no>
2022-01-12 12:51:48 +0100

    split ts and traj

---
+++ hello-xarray/cf_write_traj.py
@@ -0,0 +1,50 @@
+#%%
+from dataclasses import asdict
+from datetime import datetime, timedelta
+from os import name
+import numpy as np
+
+import xarray as xr
+from xarray_dataclasses import asdataarray, asdataset
+
+from cf_classes.attributes import DatasetAttrs
+
+from cf_classes.trajectory import TrajectoryVar
+from cf_classes.common import WGS1984
+
+#%%
+temp_traj = asdataset(
+    TrajectoryVar(
+        name="sea_water_temperature",
+        standard_name="sea_water_temperature",
+        long_name="sea_water_temperature",
+        units="degree_Celsius",
+        data=[[2, 4, 5], [90, 70, 45]],
+        time=[
+            [
+                "1970-01-01T00:00:00.000000000",
+                "1970-01-01T10:00:00.000000000",
+                "1980-01-01T10:00:00.000000000",
+            ],
+            [
+                "1990-01-01T00:00:00.000000000",
+                "1990-02-01T10:00:00.000000000",
+                "1990-03-01T10:00:00.000000000",
+            ],
+        ],
+    )
+)
+
+# %%
+temp_traj.data.sel(trajectory="second").plot(x="time").line("o")
+# %%
+temp_traj.attrs = asdict(
+    DatasetAttrs(
+        title="hei",
+        date_created=str(datetime.now()),
+        keywords=["hei"],
+        featureType="trajectory",
+    )
+)
+
+# %%
--- hello-xarray/cf_write_ts.py
+++ hello-xarray/cf_write_ts.py
@@ -86,41 +86,4 @@ ds.attrs = asdict(
 
 # %%
 ds.salinity.plot.line("o")
-# %%
-ds.to_netcdf("test.nc")
-# %%
-temp_traj = asdataset(
-    TrajectoryVar(
-        name="sea_water_temperature",
-        standard_name="sea_water_temperature",
-        long_name="sea_water_temperature",
-        units="degree_Celsius",
-        data=[[2, 4, 5], [90, 70, 45]],
-        time=[
-            [
-                "1970-01-01T00:00:00.000000000",
-                "1970-01-01T10:00:00.000000000",
-                "1980-01-01T10:00:00.000000000",
-            ],
-            [
-                "1990-01-01T00:00:00.000000000",
-                "1990-02-01T10:00:00.000000000",
-                "1990-03-01T10:00:00.000000000",
-            ],
-        ],
-    )
-)
 
-# %%
-temp_traj.data.sel(trajectory="second").plot(x="time").line("o")
-# %%
-temp_traj.attrs = asdict(
-    DatasetAttrs(
-        title="hei",
-        date_created=str(datetime.now()),
-        keywords=["hei"],
-        featureType="trajectory",
-    )
-)
-
-# %%
