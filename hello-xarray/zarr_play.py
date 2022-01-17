3211627ad4fbdb7ed831c988003d4d9c4a2a1a91
Kim Nes Leirvik <kim.leirvik@niva.no>
2022-01-17 10:03:17 +0100

    use test file

--- hello-xarray/zarr_play.py
+++ hello-xarray/zarr_play.py
@@ -4,15 +4,19 @@ import xarray as xr
 import zarr
 
 # %%
-ds = xr.open_mfdataset("./nc/*.nc")
+ds = xr.open_mfdataset("test.nc")
 # %%
 ds
 # %%
-gcs_path = "gs://niva-test-datasets/iddefjorden"
+gcs_path = "gs://niva-test-datasets/test"
 gcs = gcsfs.GCSFileSystem()
 store = gcsfs.GCSMap(root=gcs_path, gcs=gcs)
 #%%
 compressor = zarr.Blosc(cname="zstd")
 encoding = {vname: {"compressor": compressor} for vname in ds.data_vars}
 ds.to_zarr(store=store, consolidated=True)
+# %%
+ds = xr.open_zarr("gs://niva-test-datasets/test")
+# %%
+ds
 # %%
